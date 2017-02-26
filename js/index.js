e37fcad9309b143583acfbda30ae947987cb29d8
xl <2868609124@qq.com>
2017-02-26 21:03:26 +0800

    update01

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/* 
+* @Author: bk
+* @Date:   2017-02-26 20:57:45
+* @Last Modified by:   bk
+* @Last Modified time: 2017-02-26 20:58:13
+*/
+
+'use strict';
+(function(){
+	console.log('this is for the test!');
+})()
